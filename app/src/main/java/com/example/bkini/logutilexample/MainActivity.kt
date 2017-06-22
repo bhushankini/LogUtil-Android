b449368e1a6b506271c8e00fb33c1e8ece330477
Bhushan <bhushan.pralhad.kini@monsanto.com>
2017-06-22 10:23:24 -0500

    Adding math library

--- app/src/main/java/com/example/bkini/logutilexample/MainActivity.kt
+++ app/src/main/java/com/example/bkini/logutilexample/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.bkini.logutilexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logutil.LogDebug
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        LogDebug.d("Heeloo oAPAPAP")
     }
 }
---
+++ math-util/src/main/java/com/example/mathutil/MathUtil.java
@@ -0,0 +1,17 @@
+package com.example.mathutil;
+
+/**
+ * Created by bkini on 6/22/17.
+ */
+
+public class MathUtil {
+    
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int sqaure(int a){
+        return a * a;
+    }
+
+}
